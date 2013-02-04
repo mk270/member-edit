e080d88074f8f7fccf0f4037588f7591173a5010
Martin Keegan <martin@no.ucant.org>
2013-02-04 23:05:40 +0000

    parameterise port

--- src/rsdb_web/__init__.py
+++ src/rsdb_web/__init__.py
@@ -20,6 +20,7 @@ def start(config):
     htpasswd_file = config.get('server', 'htpasswd_file')
     auth_realm    = config.get('server', 'http_auth_realm')
     secret_key    = config.get('server', 'secret_key')
+    port          = int(config.get('server', 'port'))
     
     app.config['X-NAME'] = name
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///%s' % db_name
@@ -27,4 +28,4 @@ def start(config):
     app.config['HTAUTH_HTPASSWD_PATH'] = htpasswd_file
     app.secret_key = secret_key
 
-    app.run(debug=True)
+    app.run(debug=True, port=port)
